fd6226d3098956ff6a1c61dd4e38ab5819fb2a92
kasshii28 <gp22a043@oecu.jp>
2024-12-23 15:02:29 +0900

    create smallest binary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 
 //標準ライブラリ無効
 #![no_std]
+#![no_main]
 
-fn main() {
-    println!("Hello, world!");
+use core::panic::PanicInfo;
+
+// エントリポイント
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
